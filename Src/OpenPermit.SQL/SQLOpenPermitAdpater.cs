1141e88ee168750038754841390a24e249dc540b
dperaza4openpermit <dperaza24@gmail.com>
2015-08-30 13:44:15 -0400

    Adding the Skeleton of the SQL OpenPermit Adapter

---
+++ Src/OpenPermit.SQL/SQLOpenPermitAdpater.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPermit.SQL
+{
+    public class SQLOpenPermitAdpater: IOpenPermitAdapter
+    {
+
+        public List<Permit> SearchPermits(PermitFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Permit GetPermit(string permitNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PermitStatus> GetPermitTimeline(string permitNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Inspection> GetInspections(string permitNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Inspection GetInspection(string permitNumber, string inspectionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Attachment GetInspectionAttachment(string permitNumber, string inspectionId, string attachmentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contractor> GetContractors(string permitNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contractor GetContractor(string permitNumber, string contractorId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
