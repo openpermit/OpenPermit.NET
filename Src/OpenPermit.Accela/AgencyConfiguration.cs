c8ff8a8a2baa98264f8fa5a1f25aea79a2f9f283
Maykel Martin <mmartin@openpermit.org>
2015-10-02 15:37:08 -0400

    Implemented permit timeline in Accela adapter
    - Implemented permit timeline support in Accela adapter
    - Created new config objects to load "mapped" BLDS fields from config
    - Added missing MasterPermitNum to Permit.cs

---
+++ Src/OpenPermit.Accela/AgencyConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using OpenPermit;
+
+namespace OpenPermit.Accela
+{
+    public class AgencyConfiguration
+    {
+        public string[] Modules { get; set; }
+        public List<StatusMapping> Status { get; set; }
+        public List<TimelineMapping> Timeline { get; set; }
+        public List<InspectionTypeMapping> InspectionType { get; set; }
+        public List<InspectionResultMapping> InspectionResult { get; set; }
+    }
+}
